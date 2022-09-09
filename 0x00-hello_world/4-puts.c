3213a2a59cd06b034ff4512c0b1a8523f905b40d
Jksleek <kalucjohnson@gmail.com>
2022-09-09 11:17:43 -0700

    4-puts c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * * main - Entry point
+ * *
+ * * Return: Always o (Success)
+ *
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (o);
+}
