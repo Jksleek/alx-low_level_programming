2c3a099e79508d273ac9ae725f9ab45afcaf0032
Jksleek <kalucjohnson@gmail.com>
2022-09-22 06:08:40 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,23 @@
+#define MAIN_H
+
+/**
+ * file: main.h
+ * auth: Jk
+ *
+ * Desc: header file containing prototypes of all functions
+ *
+ *	used in the 0x06.c - more pointers, arrays and string directory.
+ */
+
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
